98601c692a50f788f2abc7cd003b6f858565d071
Saurabh Raj Shekhar <shekharsaurabhraj@gmail.com>
2026-08-06 19:13:17 +0530

    Backfill: Search a 2D Matrix II

---
+++ 0240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int r = 0 ;
+        int c = col - 1;
+
+        while( r < row && c >= 0 ){
+
+            int element = matrix[r][c];
+
+            if( element == target ){
+                return true;
+            }
+            else if ( element > target ){
+                c--;
+            }
+            else{
+                r++;
+            }       
+        }
+        
+        return false ;
+    }
+};
