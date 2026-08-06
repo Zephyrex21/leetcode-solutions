9e615e331eaf1112663c75aa3f86a819d8b55661
Saurabh Raj Shekhar <shekharsaurabhraj@gmail.com>
2026-08-06 19:11:23 +0530

    Backfill: Minimum Depth of Binary Tree

---
+++ 0111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        
+        // base case
+        if( root == NULL ){
+            return 0;
+        }
+
+        // if left subtree does not exist
+        if( root -> left == NULL ){
+            return minDepth( root -> right ) + 1;
+        }
+
+        // if right subtree does not exist
+        if( root -> right == NULL ){
+            return minDepth( root -> left ) + 1;
+        }
+
+        // both subtrees exist
+        int left = minDepth( root -> left );
+        int right = minDepth( root -> right );
+
+        return min( left, right ) + 1;
+    }
+};
