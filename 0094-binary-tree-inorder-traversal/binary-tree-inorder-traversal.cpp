8217832979c7b73e8f49b4bf050b4f682213e974
Saurabh Raj Shekhar <shekharsaurabhraj@gmail.com>
2026-08-06 19:10:46 +0530

    Backfill: Binary Tree Inorder Traversal

---
+++ 0094-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void inorder( TreeNode *root , vector<int>& ans ){
+
+        // left -> root -> right
+        if( root == NULL ){
+            return ;
+        }
+
+        // inorder ( root -> left )
+        inorder( root -> left , ans );
+
+        // root 
+        ans.push_back( root -> val );
+
+        // inorder( root -> right )
+        inorder( root -> right , ans  );
+
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder( root , ans );
+        return ans;
+    }
+};
