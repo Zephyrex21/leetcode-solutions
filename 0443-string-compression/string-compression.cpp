be11127c7b3fa62885fdf9621354f2cf9d36c7ad
Saurabh Raj Shekhar <shekharsaurabhraj@gmail.com>
2026-08-06 19:14:32 +0530

    Backfill: String Compression

---
+++ 0443-string-compression/string-compression.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        
+        int i = 0 ;
+        int ansIndex = 0 ;
+
+        while( i < chars.size()){
+
+            int j = i+1 ;
+
+            while(  j < chars.size()  &&  chars[i] == chars[j] ){
+                j++ ;
+            }
+
+            /// ya vector hogya hai, ya fir new/diffrent character milgaya hai 
+
+            //purana character store kar leta hu 
+            chars[ ansIndex++ ] = chars[i];
+
+            int count  = j - i ;
+
+            if( count>1 ){
+                
+                // adding  the count  value digit by digit 
+                string cnt = to_string(count);
+                
+                for( char ch : cnt ){
+                    chars[ ansIndex++ ] = ch ; 
+                }
+            }
+            i = j ;
+        }
+        return ansIndex ;
+        
+    }
+};
